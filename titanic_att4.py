1f4eb3b8f45346b0e6a0f4261c55900a4289386b
speccy-benji <kenneth.g.mackinnon@gmail.com>
2018-01-04 00:06:57 +0000

    Add files via upload
    Previous code built in code anywhere

---
+++ titanic_att4.py
@@ -0,0 +1,79 @@
+import pandas as pd
+import numpy as np
+
+import tensorflow as tf
+
+FEATURES = ["Pclass", "Age", "SibSp", "Parch", "Sex", "Embarked"]
+LABEL = "Survived"
+
+# continuous cols
+Pclass = tf.feature_column.numeric_column("Pclass")
+Age = tf.feature_column.numeric_column("Age")
+SibSp = tf.feature_column.numeric_column("SibSp")
+Parch = tf.feature_column.numeric_column("Parch")
+
+# categorial cols
+Sex = tf.feature_column.categorical_column_with_vocabulary_list(
+    "Sex", ["female", "male"])
+Embarked = tf.feature_column.categorical_column_with_vocabulary_list(
+    "Embarked", ["C", "Q", "S"])
+#buckets 
+age_buckets = tf.feature_column.bucketized_column(Age, 
+       boundaries=[18, 25, 30, 35, 40, 45, 50, 55, 60, 65])
+
+feature_cols = [Pclass, SibSp, Parch, Sex, Embarked, age_buckets]
+
+def get_data():
+  #loads the data into  dataframe then splits into training and test data
+  df = pd.read_csv("titanic_data.csv")
+  train=df.sample(frac=0.8,random_state=200)
+  test=df.drop(train.index)
+  return train, test
+
+def input_fn(df, num_epochs, shuffle):
+  # remove any NaNs
+  df = df.dropna(how="any", axis=0)
+  df_data = df[FEATURES]
+  label = df[LABEL]
+  return tf.estimator.inputs.pandas_input_fn(
+    x=df_data,
+    y=label,
+    batch_size=100,
+    num_epochs=num_epochs,
+    shuffle=shuffle)
+
+def predict_input_fn():
+  prediction_set = pd.read_csv("titanic_predictions.csv")
+
+
+  return tf.estimator.inputs.pandas_input_fn(
+    x=prediction_set,
+    num_epochs=1,
+    shuffle=False)
+
+  
+def main():
+  train, test = get_data()
+  # set up classifier
+  m = tf.estimator.LinearClassifier(
+        model_dir="/tmp/titanic",
+        feature_columns=feature_cols)
+  # train model
+  m.train(input_fn=input_fn(train, num_epochs=None, shuffle=True),
+          steps=2000)
+  # evaluate model
+  accuracy_score = m.evaluate(input_fn=input_fn(test, num_epochs=1,
+                                                shuffle=False))["accuracy"]
+  print("\nAccuracy: {0:f}\n".format(accuracy_score))
+
+  # try a prediction
+  predictions = list(m.predict(input_fn=predict_input_fn()))
+  
+  predicted_classes = [p["classes"] for p in predictions]
+  
+  print(
+      "Survival Predictions:    {}\n"
+      .format(predicted_classes))
+  
+if __name__ == "__main__":
+  main()
---
+++ titanic_att8.py
@@ -0,0 +1,96 @@
+import pandas as pd
+import numpy as np
+
+import tensorflow as tf
+
+FEATURES = ["PassengerId" ,"Pclass","Name","Sex","Age","SibSp","Parch","Ticket","Fare","Cabin","Embarked"]
+LABEL = "Survived"
+
+FILE_TRAIN = "titanic_train.csv"
+FILE_TEST = "titanic_test.csv"
+FILE_PREDICT = "titanic_predictions2.csv"
+
+def input_fn(file_path, repeat_count=1, shuffle=False):
+  # define mapping function
+  def decode_csv(line):
+    parsed_line = tf.decode_csv(line, [[0], [0], [0], [""], ["unknown"], [0.], [0], [0], [""], [0.], ["U"], ["U"]])
+    label = parsed_line[1]
+    del parsed_line[1] # removes the label
+    parsed_line[-2] = tf.substr(parsed_line[-2],0,1)
+    features = parsed_line
+    d = dict(zip(FEATURES, features)), label
+    return d
+  dataset = (tf.data.TextLineDataset(file_path)
+            .skip(1)
+            .map(decode_csv))
+  if shuffle:
+    dataset = dataset.shuffle(buffer_size=256)
+  dataset = dataset.repeat(repeat_count)
+  dataset = dataset.batch(32)
+  iterator = dataset.make_one_shot_iterator()
+  batch_features, batch_labels = iterator.get_next()
+  return batch_features, batch_labels
+
+def predict_input_fn():
+  # return to this in a moment
+  prediction_set = pd.read_csv("titanic_predictions2.csv")
+  
+  return tf.estimator.inputs.pandas_input_fn(
+    x=prediction_set,
+    num_epochs=1,
+    shuffle=False)
+
+  
+def main():
+  # continuous cols
+  Pclass = tf.feature_column.numeric_column("Pclass")
+  Age = tf.feature_column.numeric_column("Age")
+  SibSp = tf.feature_column.numeric_column("SibSp")
+  Parch = tf.feature_column.numeric_column("Parch")
+
+  # categorial cols
+  Sex = tf.feature_column.categorical_column_with_vocabulary_list(
+      "Sex", ["female", "male", "unknown"])
+  Embarked = tf.feature_column.categorical_column_with_vocabulary_list(
+      "Embarked", ["C", "Q", "S", "U"])
+  cabin_prefix = tf.feature_column.categorical_column_with_vocabulary_list(
+      "Cabin", ["A", "B", "C", "D", "E", "F", "G", "T", "U"])
+  #buckets 
+  age_buckets = tf.feature_column.bucketized_column(Age, 
+         boundaries=[18, 25, 30, 35, 40, 45, 50, 55, 60, 65])
+
+  feature_cols = [Pclass,
+                  SibSp,
+                  Parch, 
+                  tf.feature_column.indicator_column(Sex),
+                  tf.feature_column.indicator_column(Embarked),
+                  tf.feature_column.indicator_column(cabin_prefix),
+                  age_buckets]
+
+  # set up classifier
+  m = tf.estimator.DNNClassifier(
+        model_dir="/tmp/titanicDNN2",
+        feature_columns=feature_cols,
+        hidden_units=[10, 10],
+        n_classes=2)
+        
+  # train model
+  m.train(input_fn=lambda: input_fn(FILE_TRAIN, 8, True),
+          steps=2000)
+  # evaluate model
+  evaluation_results = m.evaluate(input_fn=lambda: input_fn(FILE_TEST, 1, False))
+  for key in  evaluation_results:
+    print("    {} was:   {}".format(key, evaluation_results[key]))
+
+  # try a prediction
+  predictions = m.predict(input_fn=predict_input_fn())
+  for idx, prediction in enumerate(predictions):
+    survived = prediction["class_ids"][0]
+    if survived:
+      print("Survived")
+    else:
+      print("Died")
+  
+  
+if __name__ == "__main__":
+  main()
